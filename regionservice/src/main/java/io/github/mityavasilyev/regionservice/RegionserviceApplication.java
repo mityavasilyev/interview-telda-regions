53db4cccbb996b42c10773bf8bb4ab9a2fd0f27c
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-03-25 16:52:26 +0300

    Project Setup

---
+++ regionservice/src/main/java/io/github/mityavasilyev/regionservice/RegionserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.mityavasilyev.regionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionserviceApplication.class, args);
+    }
+
+}
---
+++ regionservice/src/test/java/io/github/mityavasilyev/regionservice/RegionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mityavasilyev.regionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
