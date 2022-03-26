66f07a90b05dfcb080e1617ae25f829f7ca3ffde
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-03-26 15:19:36 +0300

    Bruhsed up a bit. Final tweaks for service tests. Closes #6

---
+++ regionservice/src/main/resources/populate.sql
@@ -0,0 +1,8 @@
+insert into REGION (REGION_NAME, REGION_SHORT_NAME)
+VALUES ('Lenoblast', 'LO');
+insert into REGION (REGION_NAME, REGION_SHORT_NAME)
+VALUES ('Moscow', 'MSC');
+insert into REGION (REGION_NAME, REGION_SHORT_NAME)
+VALUES ('St.Petersburg', 'SPB');
+insert into REGION (REGION_NAME, REGION_SHORT_NAME)
+VALUES ('Arhangelskaya Oblast', 'AO');
--- regionservice/src/main/resources/schema.sql
+++ regionservice/src/main/resources/schema.sql
@@ -8,8 +8,3 @@ create table REGION
     constraint REGIONS_PK
         primary key (ID)
 );
-
--- insert into REGION (REGION_NAME, REGION_SHORT_NAME) VALUES ( 'Lenoblast', 'LO' );
--- insert into REGION (REGION_NAME, REGION_SHORT_NAME) VALUES ( 'Moscow', 'MSC' );
--- insert into REGION (REGION_NAME, REGION_SHORT_NAME) VALUES ( 'St.Petersburg', 'SPB' );
--- insert into REGION (REGION_NAME, REGION_SHORT_NAME) VALUES ( 'Arhangelskaya Oblast', 'AO' );
