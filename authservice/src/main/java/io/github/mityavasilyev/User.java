0d50559c79af65bc6a66364c155522b9af6df528
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-03-27 14:31:37 +0300

    Implemented basic auth-service

---
+++ authservice/src/main/java/io/github/mityavasilyev/User.java
@@ -0,0 +1,23 @@
+package io.github.mityavasilyev;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String passowrd;
+}
