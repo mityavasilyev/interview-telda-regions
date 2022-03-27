930488bc9c281563548fa36c993ca62620065c78
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-03-27 16:02:07 +0300

    Refactoring

---
+++ eurekaRegistryServer/src/main/java/io/github/mityavasilyev/eurekaregistryserver/EurekaRegistryServerApplication.java
@@ -0,0 +1,15 @@
+package io.github.mityavasilyev.eurekaregistryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaRegistryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaRegistryServerApplication.class, args);
+    }
+
+}
---
+++ eurekaRegistryServer/src/test/java/io/github/mityavasilyev/eurekaregistryserver/EurekaRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mityavasilyev.eurekaregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
